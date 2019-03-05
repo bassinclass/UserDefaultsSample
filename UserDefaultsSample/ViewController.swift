43e263bc2db78c09523d2bb819660a7f0aa9c116
joe fleischauer <jfleischauer@rlas-116.org>
2019-03-05 11:27:55 -0600

    Initial commit

--- UserDefaultsSample/ViewController.swift
+++ UserDefaultsSample/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var rosterLabel: UILabel!
+    @IBOutlet weak var chosenNameLabel: UILabel!
+    
+    let defaults = UserDefaults.standard
+    var nameArray: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadNames()
     }
 
+    @IBAction func onAddButtonTapped(_ sender: Any) {
+        if let name = nameTextField.text {
+            nameArray.append(name)
+            defaults.set(nameArray, forKey: "roster")
+        }
+        rosterLabel.text = nameArray.joined(separator: ", ")
+        chosenNameLabel.text = nameArray.randomElement()
+    }
+    
+    @IBAction func onClearButtonTapped(_ sender: Any) {
+        nameArray.removeAll()
+        defaults.set(nameArray, forKey: "roster")
+        rosterLabel.text = ""
+    }
+    
+    
+    func loadNames() {
+        if let savedArray = defaults.object(forKey: "roster") as? [String] {
+            nameArray = savedArray
+        }
 
+    }
+    
+    
 }
 
