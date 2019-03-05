9e2a35f3696fa40b7ec4a95165d100e402cf2f7c
joe fleischauer <jfleischauer@rlas-116.org>
2019-03-05 08:55:13 -0600

    Initial Commit

---
+++ UserDefaultsSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDefaultsSample
+//
+//  Created by Fleischauer, Joseph on 3/5/19.
+//  Copyright © 2019 Fleischauer, Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
